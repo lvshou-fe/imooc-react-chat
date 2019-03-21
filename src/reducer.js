c7cb34fc36480f6cfa68366bfdcba51ce6c95244
jerry.wu <568109135@qq.com>
2019-03-21 19:36:25 +0800

    Update reducer.js
    //把用户聊天对象抽象为一个redux的全局对象

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,10 @@
 import {user} from './redux/user.redux'
+
+//把用户聊天对象抽象为一个redux的全局对象
 import {chatuser} from './redux/chatuser.redux'
+
 import {chat} from './redux/chat.redux'
+
 // 合并所有的reducer，并且返回
 import { combineReducers } from 'redux'
 

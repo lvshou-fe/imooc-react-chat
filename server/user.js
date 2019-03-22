10cd668f83ffca3efd826c8102a0409635c31a80
jerry.wu <568109135@qq.com>
2019-03-22 19:11:42 +0800

    Update user.js

--- server/user.js
+++ server/user.js
@@ -21,6 +21,7 @@ Router.get('/getmsglist', function (req,res) {
     userdoc.forEach(v=>{
       users[v._id] = {name: v.user, avatar:v.avatar}
     })
+    //查询 多个条件
     Chat.find({'$or':[{from:user},{to:user}]}, function (err, doc) {
       if(!err) {
         return res.json({code:0, msgs: doc, users: users})
